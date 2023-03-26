bc121c01f86639ee3501998149badbc893a465ce
hexagonium <selfeduc1234@gmail.com>
2023-03-26 13:42:00 +0500

    init repo

---
+++ dist/js/script.js
@@ -0,0 +1,22 @@
+const img = document.getElementById("img");
+const button = document.getElementById("button");
+const url = "http://aws.random.cat/meow";
+
+async function fetchHandler() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        img.src = data.file;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+button.addEventListener("click", () => {
+    let isLoaded = img.complete;
+
+    if (isLoaded) {
+        fetchHandler();
+    }
+});
